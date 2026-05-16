7e9ee05a73f0658e497734b350ac3a40f5f228c4
Arushi Khare <arushikhare0606@gmail.com>
2026-05-16 20:31:10 +0530

    Set up frontend-backend communication using CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import { connectDB } from "./lib/db.js"
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 
 
@@ -15,6 +16,11 @@ app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRoutes)
 app.use("/api/message", messageRoutes)
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+    }
+))
 
 app.listen(PORT,() => {
     console.log("Server is running on PORT:" + PORT);
